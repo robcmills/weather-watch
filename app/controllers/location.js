cbe56b9b5173f18fe8992969183e0da5337ee7d6
robcmills <robcmills@gmail.com>
2015-06-15 16:02:57 -0700

    flush out location input form, basic error handles

--- app/controllers/location.js
+++ app/controllers/location.js
@@ -2,23 +2,51 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   isGeolocationSupported: true,
-  latitude: null,
-  longitude: null,
+  isLocating: false,
+  latitude: Ember.computed.alias('position.coords.latitude'),
+  longitude: Ember.computed.alias('position.coords.longitude'),
+  position: null,
+
+  mapUrl: function(){ 
+    var lat = this.get('latitude');
+    var longitude = this.get('longitude');
+    var url = "http://maps.googleapis.com/maps/api/staticmap?center=%@,%@&zoom=14&size=400x300&sensor=false".fmt(lat, longitude);
+    return url;
+  }.property('latitude', 'longitude'),
 
   getLocation: function() {
+    if(!this.get('isGeolocationSupported')) { return; }
     var self = this;
+    this.set('isLocating', true);
     if(navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
-        self.setPosition(position);
-      });
+        self.set('position', position);
+        self.set('isLocating', false);
+      }, self.showError);
     } else {
       this.set('isGeolocationSupported', false);
     }
   },
-  setPosition: function(position) {
-    this.setProperties({
-      'latitude': position.coords.latitude,
-      'longitude': position.coords.longitude
-    });
+  showError: function(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+      console.error("User denied the request for Geolocation.");
+      break;
+      case error.POSITION_UNAVAILABLE:
+      console.error("Location information is unavailable.");
+      break;
+      case error.TIMEOUT:
+      console.error("The request to get user location timed out.");
+      break;
+      case error.UNKNOWN_ERROR:
+      console.error("An unknown error occurred.");
+      break;
+    }
+    this.set('isLocating', false);
+  },
+  actions: {
+    useCurrent: function() {
+      this.getLocation();
+    }
   }
 });
--- app/routes/location.js
+++ app/routes/location.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   setupController: function() {
     this._super();
-    console.log('location setupController');
     this.controllerFor('location').getLocation();
   }
 });
