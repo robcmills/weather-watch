4dc89f30192297d68f58b71dd1f990b1f865073c
robcmills <robcmills@gmail.com>
2015-06-15 22:20:40 -0700

    begin mapping actual data

--- app/controllers/weather.js
+++ app/controllers/weather.js
@@ -1,10 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  needs: ['api'],
-  graphData: [
-    {'x': 1, 'y': 4},
-    {'x': 2, 'y': 8},
-    {'x': 3, 'y': 6},
-  ]
+  needs: ['api', 'weathers']
 });
--- app/controllers/weathers.js
+++ app/controllers/weathers.js
@@ -3,5 +3,19 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   needs: ['weather'],
   model: Ember.computed.oneWay('controllers.weather.json.list'),
-  itemController: 'weather'
+  itemController: 'weather',
+
+  K2F: function(kelvin) {
+    return (kelvin - 273.15) * 1.8 + 32.0;
+  },
+
+  graphData: function() {
+    var self = this;
+    return this.map(function(item) {
+      return { 
+        'x': item.get('model.dt'), 
+        'y': self.K2F(item.get('model.main.temp'))
+      };
+    });
+  }.property('model')
 });
