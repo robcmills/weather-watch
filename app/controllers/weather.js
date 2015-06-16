ea156fa742e2d1f6aad8b27705e4bc5d6f6dea64
robcmills <robcmills@gmail.com>
2015-06-15 21:58:40 -0700

    add nf-graph with mock data, make location and weather layouts more consistent

--- app/controllers/weather.js
+++ app/controllers/weather.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  needs: ['api']
+  needs: ['api'],
+  graphData: [
+    {'x': 1, 'y': 4},
+    {'x': 2, 'y': 8},
+    {'x': 3, 'y': 6},
+  ]
 });
