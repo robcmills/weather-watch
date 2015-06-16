48cc5b1c22d1455f2a84749572ab31f907dfbca9
robcmills <robcmills@gmail.com>
2015-06-15 21:32:39 -0700

    add api controller

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  needs: ['location'],
-  lat: Ember.computed.oneWay('controllers.location.latitude'),
-  lon: Ember.computed.oneWay('controllers.location.longitude')
 });
