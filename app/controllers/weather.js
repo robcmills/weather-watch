e54f98edd1a485cf805e5a9fe9e81dee5a0fb772
robcmills <robcmills@gmail.com>
2015-06-15 20:41:48 -0700

    draft update ajax

--- app/controllers/weather.js
+++ app/controllers/weather.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   needs: ['location'],
-  queryParams: ['lat', 'lon'],
+  queryParams: ['lat', 'lon', 'start', 'end'],
   apiKey: '5074b6f4157fe2178602f818e15e7c9b', // openWeatherMap
   apiBase: "http://api.openweathermap.org/data/2.5/history/city",
   startAt: null,
@@ -10,12 +10,26 @@ export default Ember.Controller.extend({
   // lat: Ember.computed.oneWay('controllers.location.latitude'),
   // lon: Ember.computed.oneWay('controllers.location.longitude'),
 
+  unixStart: function() {
+    var startAt = this.get('startAt');
+    return (startAt ? moment(startAt).unix() : undefined);
+  }.property('startAt'),
+  unixEnd: function() {
+    var endAt = this.get('endAt');
+    return (endAt ? moment(endAt).unix() : undefined);
+  }.property('endAt'),
+
   apiQueryParams: function() {
-    var queryParams = [];
-    queryParams.push('lat=' + this.get('lat'));
-    queryParams.push('lon=' + this.get('lon'));
-    return '&'.join(queryParams);
-  }.property('lat', 'lon'),
+    var queryParams = [],
+      lat = this.get('lat'), lon = this.get('lon'),
+      start = this.get('unixStart'), end = this.get('unixEnd');
+    queryParams.push('APPID=' + this.get('apiKey'));
+    if(lat){ queryParams.push('lat=' + this.get('lat')); }
+    if(lon){ queryParams.push('lon=' + this.get('lon')); }
+    if(start){ queryParams.push('start=' + this.get('unixStart')); }
+    if(end){ queryParams.push('end=' + this.get('unixEnd')); }
+    return queryParams.join('&');
+  }.property('lat', 'lon', 'unixStart', 'unixEnd'),
 
   apiUrl: function() {
     return this.get('apiBase') + '?' + this.get('apiQueryParams');
@@ -24,6 +38,18 @@ export default Ember.Controller.extend({
   actions: {
     update: function() {
       console.log('update');
+      var self = this;
+      this.set('jqxhr', Ember.$.getJSON(this.get('apiUrl'), function(json) {
+          console.log('success', json);
+        }).done(function(json) {
+          console.log('done', json);
+          self.set('content', json);
+        }).fail(function(jqxhr, textStatus, error) {
+          console.log('fail', textStatus, error);
+        }).always(function() {
+          console.log('always');
+        })
+      );
     }
   }
 });
--- app/routes/weather.js
+++ app/routes/weather.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // model: function(params) {
+  //   console.log('weather model params', params);
+  //   return Ember.$.getJSON(this.controllerFor('weather').get('apiUrl'));
+  // }
 });
